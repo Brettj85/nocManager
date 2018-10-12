9dae915ab94ff8484332157aa4c5e6a00199c0c4
brett <brettj85@gmail.com>
2018-10-12 16:02:24 -0500

    added files

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ function divSelector(selection){
     var calendarSelection = document.getElementById("calendar");
     var noticesSelection = document.getElementById("notices");
     var toolsSelection = document.getElementById("tools");
-    var careerSelection = document.getElementById("career");
+    var diviceLookupSelection = document.getElementById("diviceLookup");
     var currentSelection = document.getElementById(selection);
     calendarSelection.style.display = "none";
     noticesSelection.style.display = "none";
     toolsSelection.style.display = "none";
-    careerSelection.style.display = "none";
+    diviceLookupSelection.style.display = "none";
     homeSelection.style.display = "none";
     currentSelection.style.display = "flex";
 }
